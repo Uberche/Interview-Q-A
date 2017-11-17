21705afa02936271c7b8e4f021065a126505e53b
Ethan Strauss <estrauss@gmail.com>
2017-11-17 17:01:19 -0500

    Create fizzbuzz.js
    fizzbuzz practice.

---
+++ Fizz Buzz.js
@@ -0,0 +1,13 @@
+function fizzBuzz() {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+      console.log(i + "fizzbuzz");
+    } else if ( i % 5 === 0) {
+      console.log(i + "buzz");
+    } else if ( i % 3 === 0) {
+      console.log(i + "fizz");
+    } 
+  }
+}
+
+fizzBuzz(1);
